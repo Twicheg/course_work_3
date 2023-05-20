c45f0ebe32a6aa9abe4bad5ae6a8b3e01b88bcdf
eliseev <twicheg@gmail.com>
2023-05-20 14:18:14 +0300

    add utils.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-if __name__=='__main__':
-    PATH=''
+import utils as ut
+
+
+def main(PATH):
+    operation = ut.Bank(PATH)
+    operation.list_of_last_five = operation.open_json(operation.last_of_transaction)
+    # operation.final(operation.list_of_last_five, operation.python_list)
+    print(operation.list_of_last_five, operation.python_list)
+
+
+if __name__ == '__main__':
+    PATH = 'operations.json'
     main(PATH)
---
+++ utils.py
@@ -0,0 +1,34 @@
+import json
+
+
+class Bank:
+    def __init__(self, PATH, last_of_transaction=5):
+        self.PATH = PATH
+        self.last_of_transaction = last_of_transaction
+        self.python_list = None
+        self.list_of_last_five = None
+
+    def open_json(self, last_of):
+        try:
+            with open(self.PATH) as file:
+                file = json.loads(file.read())
+        except BaseException:
+            return []
+        finally:
+            self.python_list = file
+            return self.sort_collection(self.python_list, last_of)
+
+    def sort_collection(self, python_list, last_of):
+        date_list = []
+        for dicts in python_list:
+            try:
+                if dicts["state"] == "EXECUTED":
+                    date_list.append(dicts["date"])
+            except KeyError:
+                continue
+        date_list.sort(reverse=True)
+        return date_list[:last_of]
+    def ready_for_print(self,python_list,last_of_list):
+        pass
+    def __repr__(self):
+        return f''
